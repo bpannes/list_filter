2305ebcc0b18913cbe35a1fe8f40089fb339fe93
Bruno Annes <bruno.annes@sumup.com>
2021-04-07 14:47:11 -0300

    logic and test done

--- lib/list_filter.ex
+++ lib/list_filter.ex
@@ -1,18 +1,12 @@
 defmodule ListFilter do
-  @moduledoc """
-  Documentation for `ListFilter`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ListFilter.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def call(list) do
+    list
+    |> Enum.map(fn elem -> to_string(elem) end)
+    |> Enum.map(fn elem -> Integer.parse(elem) end)
+    |> Enum.map(fn
+      {num, ""} -> num |> rem(2) |> abs
+      :error -> 0
+    end)
+    |> Enum.sum()
   end
 end
